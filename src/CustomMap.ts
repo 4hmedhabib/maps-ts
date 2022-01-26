837bed17ecdfe5b21571b7bd2ffa2a7e8be3a91c
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-26 13:33:35 +0300

    added user market

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,15 +1,32 @@
 /// <reference types="@types/google.maps" />
+import {User} from './User'
+import {Company} from './Company'
 export class CustomMap {
     private googleMap: google.maps.Map;
 
-    constructor () {
+    constructor() {
         this.googleMap = new google.maps.Map(document.getElementById('map'), {
-            zoom:1 ,
+            zoom: 1,
             center: {
                 lat: 0,
                 lng: 0
             }
         })
+
+    }
+
+    addUserMarker(user: User) {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: user.location.lat,
+                lng: user.location.lng
+            }
+        })
+    }
+
+    addCompanyMarker(compnay: Company) {
         
     }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 /// <reference types="@types/google.maps" />
 
-// import { Company } from './Company';
-// import {User} from './User';
+import { Company } from './Company';
+import {User} from './User';
 import {CustomMap} from './CustomMap'
 
-// const user = new User();
-// const compnay = new Company(); 
+const user = new User();
+const compnay = new Company(); 
 
-new CustomMap();
+console.log(user)
+console.log(compnay)
+
+const customMap = new CustomMap();
+customMap.addUserMarker(user);
