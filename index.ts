2fef2de11fa8e17f449208df909434748de61b31
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-26 12:25:21 +0300

    added some files

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
+/// <reference types="@types/google.maps" />
+
 // import { Company } from './Company';
 // import {User} from './User';
 
 // const user = new User();
-
 // const compnay = new Company(); 
 
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+})
